3fc7e9330a99ec486a7fb4afb6a34e34d17ed835
sajjadbadshah708-design <sajjadbadshah708@gmail.com>
2025-09-17 23:41:44 +0500

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,3 +1,9 @@
-git add.
-git commit -m "First commit"
-  git push origin main
+echo '#include
+using namespace std;
+int main() {
+cout << "Ubaid Ullah" << endl;
+return 0;
+}' > hello.cpp
+git add .
+git commit -m "Updated to print my name"
+git push origin main
